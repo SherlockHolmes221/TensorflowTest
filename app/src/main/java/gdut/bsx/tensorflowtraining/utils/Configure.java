ed483f549c5098b4ff49f21cf07c48b5f9c83736
屈贤 <0976162258@qq.com>
2019-05-14 18:19:31 +0800

    add yoga model

--- app/src/main/java/gdut/bsx/tensorflowtraining/utils/Configure.java
+++ app/src/main/java/gdut/bsx/tensorflowtraining/utils/Configure.java
@@ -7,7 +7,6 @@ public class Configure {
     public static final float IMAGE_STD = 1;
     public static final String INPUT_NAME = "Mul";
     public static final String OUTPUT_NAME = "final_result";
-    public static final String MODEL_FILE = "file:///android_asset/model/optimized_graph.pb";
+    public static final String MODEL_FILE = "file:///android_asset/model/yoga_optimized_graph.pb";
     public static final String LABEL_FILE = "file:///android_asset/model/retrained_labels.txt";
-
 }
